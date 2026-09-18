da0b7a58eae65af98d9ef9da64e52f0fdb3cdbd4
bart234 <bartlomiej.p.biernacki@o2.pl>
2026-09-18 20:00:43 +0200

    feat(service-notification-center):switch from hardcoded env to sys_envs

--- ServiceNotificationCenter/settings/redis_cfg.py
+++ ServiceNotificationCenter/settings/redis_cfg.py
@@ -1,6 +1,7 @@
+import os
 
-redis_cfg = {   'REDIS_TIME_TO_EXPIRE_KEY' : 20,
-                'REDIS_HOST' : "inf_redis",      
-                'REDIS_PORT' : "6379"}   
- #os.getenv("REDIS_HOST","redis")
-#  os.getenv("REDIS_PORT","6379")}
+
+redis_cfg = {   'REDIS_TIME_TO_EXPIRE_KEY' :    os.getenv("REDIS_TIME_TO_EXPIRE_KEY",20),
+                'REDIS_HOST' :                  os.getenv("REDIS_HOST","inf_redis"),      
+                'REDIS_PORT' :                  os.getenv("REDIS_PORT","6379")
+            }   
